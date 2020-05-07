d92d7867dd8dc9d63bac2271cf0392ea66d0c67f
nasjones <nasjonez1@gmail.com>
2020-05-06 20:15:54 -0400

    api fixed

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,10 +3,11 @@ const { API_KEY } = require('./config')
 const logger = require('./logger')
 
 function validateBearerToken(req, res, next) {
-    console.log(req.header('authorization'))
+    // console.log(req.get('Authorization'))
     const authToken = req.get('Authorization')
     logger.error(`Unauthorized request to path: ${req.path}`)
-    console.log(req)
+    console.log(authToken.split(' ')[1])
+    console.log(API_KEY)
     if (!authToken || authToken.split(' ')[1] !== API_KEY) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
